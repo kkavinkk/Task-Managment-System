4b5facca48076a697641ab875ed7223e687eaae0
kkavinkk <gk.kavin05@gmail.com>
2025-01-30 10:38:19 -0700

    learinging takes tim

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const taskAdder = document.querySelector(".add-task")
 const addBttn = document.querySelector(".add-task button")
 const tasks = document.querySelector(".tasks")
 const doneTask = document.querySelector(".checkbox")
+
+let inputData = ""
+let page = 2;
