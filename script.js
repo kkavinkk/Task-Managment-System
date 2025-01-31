9d62968fefa4d6b906a5086f74164dbb675cfc9a
kkavinkk <gk.kavin05@gmail.com>
2025-01-31 10:36:18 -0700

    starting js we moovin

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Add tasks
+    const inputTask = document.querySelector(".add-task input")
+    const taskBttn = document.querySelector(".add-task button")
+    const tasks = document.querySelector(".tasks")
     // type in a task in the input box
+    
+
     //Press the button to make the task show up below
 
 // Once a task is checked make it go away(another clear button?, Or a switch we finna find out)
