892eccca86ee5617ab480e86fc79472f17eea3e4
kkavinkk <gk.kavin05@gmail.com>
2025-02-01 12:32:46 -0700

    mmaking progress able to add tasks just need to fix how it looks innit

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 // Add tasks
     const inputTask = document.querySelector(".add-task input")
     const taskBttn = document.querySelector(".add-task button")
-    const tasks = document.querySelector(".tasks")
+    const tasks = document.querySelector("#taskList")
     // type in a task in the input box
-    let newTask = ""
-    function addTask(newTask) { 
-        newTask = inputTask.value;
-        console.log(newTask)
+    function addTask(newTask) {
+        //get input value and delete extra space 
+        const taskText = inputTask.value.trim();
 
+        if (taskText === "") return; // stops from adding a task with nothing
+
+        const label = document.createElement("label");
+        label.classList.add("container");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = false; // Default unckecked
+
+        const checkmark = document.createElement("div");
+        checkmark.classList.add("checkmark");
+
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add("tasks");
+        taskDiv.textContent = taskText;
+
+        label.appendChild(checkbox);
+        label.appendChild(checkmark);
+        label.appendChild(taskDiv);
+        taskList.appendChild(label);
+
+        inputTask.value = "";
         
     //Press the button to make the task show up below
     }
+    taskBttn.addEventListener("click", addTask)
 // Once a task is checked make it go away(another clear button?, Or a switch we finna find out)
 
 // Make the Clear button clear everything
